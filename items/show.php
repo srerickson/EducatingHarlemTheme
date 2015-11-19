833cfde64f8369502011625d7475c3727d5c5107
Seth Erickson <sr.erickson@gmail.com>
2015-11-19 12:57:30 -0700

    a few layout tweaks for show item page

--- items/show.php
+++ items/show.php
@@ -6,27 +6,43 @@
 
 ?>
 
-<div id="item-title-wrapper">
+<div id="title-row">
   <h2 class="item-title">
     <?php echo metadata('item', array('Dublin Core','Title')); ?>
   </h2>
 </div>
 
 
-<div id="primary">
-
+<div id="main-row">
 
   <div id="item-metadata">
+
+    <div class='description'>
+      <?php echo metadata('item', array('Dublin Core', 'Description')); ?>
+    </div>
+
     <?php echo all_element_texts('item', array(
         'show_element_sets' => array('Dublin Core'),
         'show_element_set_headings' => false
       )
     ); ?>
+
+    <h2>Additional Metadata</h2>
+
+    <?php echo all_element_texts('item', array(
+        'show_element_sets' => array('Item Type Metadata'),
+        'show_element_set_headings' => false
+      )
+    ); ?>
+
+
   </div>
 
 
   <!-- Files -->
   <?php
+
+    // TODO exclude non-images
     function img_object($file){
       $url = file_display_url($file);
       $path = FILES_DIR . '/' . $file->getStoragePath();
@@ -46,7 +62,9 @@
   ?>
 
   <?php if (count($item->getFiles())>0): ?>
-    <div id="seadragon-viewer"></div>
+    <div id="seadragon-wrapper">
+      <div id="seadragon-viewer"></div>
+    </div>
 
     <script type="text/javascript">
       var viewer = OpenSeadragon({
@@ -61,12 +79,12 @@
 
 
 
- <?php if(metadata('item','Collection Name')): ?>
+  <?php if(metadata('item','Collection Name')): ?>
     <div id="collection" class="element">
       <h3><?php echo __('Collection'); ?></h3>
       <div class="element-text"><?php echo link_to_collection_for_item(); ?></div>
     </div>
- <?php endif; ?>
+  <?php endif; ?>
 
    <!-- The following prints a list of all tags associated with the item -->
   <?php if (metadata('item','has tags')): ?>
@@ -89,6 +107,8 @@
       <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
   </ul>
 
-</div> <!-- End of Primary. -->
+
+</div> <!-- row -->
+
 
  <?php echo foot(); ?>
