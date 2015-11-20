ecd2a5406b054de11d082223a54609a605553a10
Seth Erickson <sr.erickson@gmail.com>
2015-11-19 23:54:10 -0700

    add file list below image gallery, left-hand column

--- items/show.php
+++ items/show.php
@@ -9,50 +9,91 @@
 <div id="main-row">
 
 
-  <!-- Images Files -->
-  <?php
 
-    // build list of item images
-    $imgs = array();
-    foreach ($item->getFiles() as $i => $file) {
-      if($file->has_derivative_image){
-        array_push($imgs, $file);
+  <div id="item-media">
+
+
+
+    <!-- Images Files -->
+    <?php
+
+      // build list of item images
+      $files = $item->getFiles();
+      $imgs = array();
+      foreach ($files as $i => $file) {
+        if($file->has_derivative_image){
+          array_push($imgs, $file);
+        }
       }
-    }
 
-    // return's seadragon tile config for image/file
-    function img_object($file){
-      $url = file_display_url($file);
-      $path = FILES_DIR . '/' . $file->getStoragePath();
-      $dims = getimagesize($path);
-      $format = "{
-        type: 'legacy-image-pyramid',
-        levels: [{
-          url: '%s',
-          width: %d,
-          height: %d
-        }]
-      }";
-      return sprintf($format,$url,$dims[0],$dims[1]);
-    }
-    $sd_tiles = join( ',', array_map('img_object', $imgs));
-    $sd_seqMode = count($imgs) > 1 ? 'true' : 'false';
-  ?>
+      // return's seadragon tile config for image/file
+      function img_object($file){
+        $url = file_display_url($file);
+        $path = FILES_DIR . '/' . $file->getStoragePath();
+        $dims = getimagesize($path);
+        $format = "{
+          type: 'legacy-image-pyramid',
+          levels: [{
+            url: '%s',
+            width: %d,
+            height: %d
+          }]
+        }";
+        return sprintf($format,$url,$dims[0],$dims[1]);
+      }
+      $sd_tiles = join( ',', array_map('img_object', $imgs));
+      $sd_seqMode = count($imgs) > 1 ? 'true' : 'false';
+    ?>
 
-  <?php if (count($imgs)>0): ?>
-    <div id="seadragon-wrapper">
-      <div id="seadragon-viewer"></div>
+    <?php if (count($imgs)>0): ?>
+      <div id="seadragon-wrapper">
+        <div id="seadragon-viewer"></div>
+      </div>
+      <script type="text/javascript">
+        var viewer = OpenSeadragon({
+          id: "seadragon-viewer",
+          prefixUrl: "../../themes/EducatingHarlemTheme/javascripts/vendor/openseadragon-2.0.0/images/",
+          tileSources: [<?php echo $sd_tiles ?>],
+          sequenceMode: <?php echo $sd_seqMode ?>,
+          showReferenceStrip: <?php echo $sd_seqMode ?>
+        });
+      </script>
+    <?php endif ?>
+
+
+    <?php if(count($files) > 0): ?>
+      <div class="element-set" id="item-files">
+        <div class="element">
+          <h3><?php echo __('All Files'); ?></h3>
+          <ul>
+            <?php foreach ($item->getFiles() as $i => $f): ?>
+              <li>
+                <a href="<?php echo $f->getWebPath() ?>">
+                  <?php echo $f->getDisplayTitle(); ?>
+                </a>
+              </li>
+            <?php endforeach ?>
+          </ul>
+        </div>
+      </div>
+    <?php endif?>
+
+
+    <!-- The following prints a citation for this item. -->
+    <div class="element-set">
+      <div id="item-citation" class="element">
+        <h3><?php echo __('Citation'); ?></h3>
+        <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
+      </div>
     </div>
-    <script type="text/javascript">
-      var viewer = OpenSeadragon({
-        id: "seadragon-viewer",
-        prefixUrl: "../../themes/EducatingHarlemTheme/javascripts/vendor/openseadragon-2.0.0/images/",
-        tileSources: [<?php echo $sd_tiles ?>],
-        sequenceMode: <?php echo $sd_seqMode ?>,
-        showReferenceStrip: <?php echo $sd_seqMode ?>
-      });
-    </script>
-  <?php endif ?>
+
+
+
+
+  </div> <!-- media column -->
+
+
+
 
 
 
@@ -95,14 +136,6 @@
     </div>
     <?php endif;?>
 
-
-    <!-- The following prints a citation for this item. -->
-    <div class="element-set">
-      <div id="item-citation" class="element">
-        <h3><?php echo __('Citation'); ?></h3>
-        <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
-      </div>
-    </div>
 
   </div> <!-- Metadata column -->
 
